74b98231cf0145ab4affe5a9f848aa465fde9815
jonathanliuuchicago <66703891+jonathanliuuchicago@users.noreply.github.com>
2021-02-05 23:11:13 -0600

    buttons

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 var today = new Date();
 
 function displayTime() {
-    document.getElementById("time").innerHTML = today;
+    if (document.getElementById("time").innerHTML=="Show Current Date and Time") {
+        document.getElementById("time").innerHTML = "Hide Current Date and Time";
+        document.getElementById("time_text").innerHTML = today;
+
+    } else {
+        document.getElementById("time").innerHTML = "Show Current Date and Time";
+        document.getElementById("time_text").innerHTML= "";
+    }
 }
 
 function displayMinutes() {
-    document.getElementById("minutes").innerHTML = today.getMinutes();
+    if (document.getElementById("minute").innerHTML=="Show Minutes") {
+        document.getElementById("minute").innerHTML = "Hide Minutes";
+        document.getElementById("minute_text").innerHTML = today.getMinutes();
+
+    } else {
+        document.getElementById("minute").innerHTML = "Show Minutes";
+        document.getElementById("minute_text").innerHTML= "";
+    }
 }
 
 function hideVideo() {
-    document.getElementById("video").style.visibility = "hidden";
+    if (document.getElementById("hide").innerHTML=="Hide Video") {
+        document.getElementById("video").style.visibility = "hidden";
+        document.getElementById("hide").innerHTML= "Show Video";
+
+    } else {
+        document.getElementById("video").style.visibility = "visible";
+        document.getElementById("hide").innerHTML= "Hide Video";
+    }
 }
